c6a7eb71db72efe3d99b7e3602ace110c14829bd
GagandeepLubana <gagandeeplubana1221@gmail.com>
2023-08-30 20:52:29 -0400

    Added cart summary card.

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -10,7 +10,7 @@ import {
 } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { FaTrash } from "react-icons/fa";
-
+import { addToCart } from "../slices/cartSlice";
 import React from "react";
 import Message from "../components/Message";
 
@@ -20,6 +20,10 @@ const CartScreen = () => {
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+
+  const addToCartHandler = (product, qty) => {
+      dispatch(addToCart({...product, qty}));
+  }
 
   return (
     <Row>
@@ -44,7 +48,7 @@ const CartScreen = () => {
                             ${item.price}
                         </Col>
                         <Col md={2}>
-                        <Form.Control as='select' value={item.qty} onChange={(e) => {}}>
+                        <Form.Control as='select' value={item.qty} onChange={(e) => addToCartHandler(item, Number(e.target.value))}>
                                             {[...Array(item.countInStock).keys()].map((x) => (
                                                 <option key={x+1} value={x+1}>
                                                     {x+1}
@@ -64,6 +68,23 @@ const CartScreen = () => {
             ))}
           </ListGroup>
         )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({ cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
+              </h2>
+              ${ cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button type='button' className='btn-block' disabled={ cartItems.length === 0}>
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
   );
