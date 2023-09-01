93ecf4c0552cb683276a1ba6c23cc53930792633
GagandeepLubana <gagandeeplubana1221@gmail.com>
2023-09-01 14:20:03 -0400

    created login slice and component

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if (user) {
         generateToken(res, user._id);
-        res.status(201).json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -72,14 +72,43 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/profile
 // @access Public
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(400);
+        throw new Error('User not found')
+    }
 });
 
 // @desc Update user profile
 // @route PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updateUser = await user.save();
+
+        res.status(200).json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email
+        })
+    }
 });
 
 // @desc Get users
